a24e1ef8de060015be8edc8f31cbf9fb10d646c7
Kristina <kristin.petrivska@gmail.com>
2024-07-30 15:13:09 -0600

    format selection

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-'use strict';
+"use strict";
 
-//debugger; 
+//debugger;
 
-function getValidatedInput (){
-const input = prompt('Enter a number that is no less than 100.');
-if (!input?.trim()){
-    alert ('Your input is canceled or empty.');
+function getValidatedInput() {
+  const input = prompt("Enter a number that is no less than 100.");
+  if (!input?.trim()) {
+    alert("Your input is canceled or empty.");
     return null;
-}
-return input;
+  }
+  return input;
 }
 
-
-function iterateInput (iterationsAmount){
-    for (let i=1; i <= iterationsAmount; i++){
+function iterateInput(iterationsAmount) {
+  for (let i = 1; i <= iterationsAmount; i++) {
     const userInput = getValidatedInput();
     if (userInput === null) {
-    return;
+      return;
     }
-    if (+userInput < 100 && i < iterationsAmount){
-        alert('The number has to be no less than 100. Please try again.');
-    }else{
-        console.log(userInput)
-        return userInput;
+    if (+userInput < 100 && i < iterationsAmount) {
+      alert("The number has to be no less than 100. Please try again.");
+    } else {
+      console.log(userInput);
+      return userInput;
     }
-    }
+  }
 }
 
-const result = iterateInput(10)
-
+const result = iterateInput(10);
