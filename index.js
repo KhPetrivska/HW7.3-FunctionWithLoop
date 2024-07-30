105d4f3ec6d77b5ae59d79ef917e1475880077e7
Kristina <kristin.petrivska@gmail.com>
2024-07-29 18:24:02 -0600

    add js code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+'use strict';
+
+//debugger; 
+
+function getValidatedInput (){
+const input = prompt('Enter a number that is no less than 100.');
+if (!input?.trim()){
+    alert ('Your input is canceled or empty.');
+    return null;
+}
+return input;
+}
+
+
+function iterateInput (iterationsAmount){
+    for (let i=1; i <= iterationsAmount; i++){
+    const userInput = getValidatedInput();
+    if (userInput === null) {
+    return;
+    }
+    if (+userInput < 100 && i < iterationsAmount){
+        alert('The number has to be no less than 100. Please try again.');
+    }else{
+        console.log(userInput)
+        return userInput;
+    }
+    }
+}
+
+const result = iterateInput(10)
 
